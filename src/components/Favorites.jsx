ec1517f1d46490a097158b06b5b40f57aad31e63
Nejciny <116084048+Nejciny@users.noreply.github.com>
2023-01-23 17:44:47 +0100

    123
    123

--- src/components/Favorites.jsx
+++ src/components/Favorites.jsx
@@ -1,14 +1,53 @@
 import { useState, useEffect} from "react"
 import { useGlobalContext } from "../context"
 
+import Slider from "react-slick"
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
 
 const Favorites = () => {
 
 
     const {favoriteBook, selectBook, CheckProperty, FavoriteBook} = useGlobalContext()
 
+    // console.log('favorite books: '+favoriteBook.length)
+
     
-    console.log('favorite books: '+favoriteBook.length)
+    const settings = {
+        dots: false,
+        infinite: false,
+        speed: 500,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        initialSlide: 0,
+        responsive: [
+          {
+            breakpoint: 1000,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2,
+              infinite: true,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              initialSlide: 2,
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+        ]
+    };
+    
 
     return(
         <div className="favorites favorites-container">
@@ -23,19 +62,22 @@ const Favorites = () => {
                
             <div className="favorites-content">
 
-                {favoriteBook.map((book) => {
-                    
-                    console.log(book)
+                <Slider {...settings}>
 
-                    return (
-                        <div className="favorite-book" >
-                            <img src={CheckProperty(book.volumeInfo, 'imageLinks')?book.volumeInfo.imageLinks.thumbnail:''} 
-                            onClick={()=>{selectBook(book.id)}} /> 
-                            <h5 >{CheckProperty(book.volumeInfo, 'title')?book.volumeInfo.title:'title'}</h5>
-                            <button id={book.id} className='remove-btn' onClick={ () => FavoriteBook(book.id) } > X </button>
-                        </div>
-                    )
-                })}
+                    {favoriteBook.map((book) => {
+                        
+                        console.log(book)
+
+                        return (
+                            <div className="favorite-book" >
+                                <img src={CheckProperty(book.volumeInfo, 'imageLinks')?book.volumeInfo.imageLinks.thumbnail:''} 
+                                onClick={()=>{selectBook(book.id)}} /> 
+                                <h5 >{CheckProperty(book.volumeInfo, 'title')?book.volumeInfo.title:'title'}</h5>
+                                <button id={book.id} className='remove-btn' onClick={ () => FavoriteBook(book.id) } > X </button>
+                            </div>
+                        )
+                    })}
+                </Slider>
             </div>
 
             
